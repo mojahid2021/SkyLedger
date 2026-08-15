2f4ecfcd5e80220c5fb02e0c8562455b54955750
Md Mojahid <aammojahid@gmail.com>
2026-08-15 10:18:22 +0600

    chore: remove obsolete agent and design documentation files

--- fix_icons.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/import {.*} from "@phosphor-icons\/react"/import { ArrowLeft, Save, Trash2, Mail, Phone, CalendarCheck, Coins, Plane, ShieldAlert, User as UserIcon, XCircle, CheckCircle, ClockCounterClockwise, ShieldCheck, UserCircle } from "@phosphor-icons\/react"/g' src/app/admin/users/[id]/page.tsx
